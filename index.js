701ecab089f90458a2698fcd033e0e2057ab01cc
0pama <95030993+0pama@users.noreply.github.com>
2024-03-19 08:06:39 -0700

    added form validation

--- index.js
+++ index.js
@@ -5,19 +5,41 @@ pass.forEach((input, index) => {
         let one = pass[0].value;
         let two = pass[1].value;
 
-        if (one === two) {
+        if (one === two && one != "") {
             document.querySelector(".warning-msg").style.display = "none"
-            document.querySelector(".match-msg").style.display = "block"
+            document.querySelector(".match-msg").style.display = "none"
             pass.forEach(element => element.classList.remove("warning"));
             pass.forEach(element => element.classList.add("match"));
         } else {
-            document.querySelector(".warning-msg").style.display = "block"
+            document.querySelector(".warning-msg").style.display = "none"
             document.querySelector(".match-msg").style.display = "none"
 
             pass.forEach(element => element.classList.add("warning"));
             pass.forEach(element => element.classList.remove("match"));
         }
     });
+
+
+
+    input.addEventListener("blur", (e) => {
+        let one = pass[0].value;
+        let two = pass[1].value;
+
+        if (one === two) {
+            document.querySelector(".warning-msg").style.display = "none"
+            document.querySelector(".match-msg").style.display = "none"
+            pass.forEach(element => element.classList.remove("warning"));
+            pass.forEach(element => element.classList.remove("match"));
+        } else {
+            document.querySelector(".warning-msg").style.display = "none"
+            document.querySelector(".match-msg").style.display = "none"
+
+            pass.forEach(element => element.classList.remove("warning"));
+            pass.forEach(element => element.classList.remove("match"));
+        }
+    });
+
+
 });
 
 
